381e5e361e93f683b55cb14d0f6db5a08501bbbc
1234 <a1234@MacBook-Pro-1234.local>
2023-11-01 15:25:17 +0300

    (add): declarative models user, orders

---
+++ models.py
@@ -0,0 +1,50 @@
+from database import Base
+from sqlalchemy import Column, Integer, Boolean, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(25), unique=True)
+    email = Column(String(80), unique=True)
+    password = Column(Text, nullable=True)
+    is_staff = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+    orders = relationship("Order", back_populates="user")
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+class Order(Base):
+
+    ORDERS_STATUS = (
+        ("PENDING", 'pending'),
+        ("IN-TRANSIT", 'in-transit'),
+        ("DELIVERED", "delivered")
+    )
+
+    PIZZA_SIZES = (
+        ("SMALL", "small"),
+        ("MEDIUM", "medium"),
+        ("LARGE", "large"),
+        ("EXTRA-LARGE", 'extra-large')
+    )
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, nullable=False)
+    order_status = Column(ChoiceType(choices=ORDERS_STATUS), default="PENDING")
+    pizza_sizes = Column(ChoiceType(choices=PIZZA_SIZES), default="SMALL")
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User", back_populates="order")
+
+    def __repr__(self):
+        return f"<Order {self.id}>"
+
+
+
+
+
+
