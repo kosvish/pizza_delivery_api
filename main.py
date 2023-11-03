1d96477a362c4ccd4da15811a0fa7d2def10dab4
1234 <a1234@MacBook-Pro-1234.local>
2023-11-03 13:20:58 +0300

    (style): fix indentation

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from auth_routes import auth_route
 from order_routes import order_route
 
+
 app = FastAPI()
+
+
+
+
 
 app.include_router(auth_route)
 app.include_router(order_route)
