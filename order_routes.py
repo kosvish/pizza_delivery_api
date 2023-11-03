1018c0b323f3124bacc3cceae97ecd26316ecb9e
1234 <a1234@MacBook-Pro-1234.local>
2023-11-03 13:13:03 +0300

    (add): oauth2 Depends

--- order_routes.py
+++ order_routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-
+from auth_routes import get_current_user
+from fastapi import Depends
+from models import User
 order_route = APIRouter(
     prefix="/orders",
     tags=["orders"]
@@ -7,5 +9,5 @@ order_route = APIRouter(
 
 
 @order_route.get("/order")
-async def hell():
+async def hell(data: User = Depends(get_current_user)):
     return {"message": "Hello Order"}
