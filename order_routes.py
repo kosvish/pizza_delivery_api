0cbd6f5e56a2f4a19e6ef6fe4abbf981a1baddaa
make_wish <mr.kocta@bk.ru>
2023-11-09 16:07:00 +0300

    (create): routes to place order

--- order_routes.py
+++ order_routes.py
@@ -1,13 +1,56 @@
-from fastapi import APIRouter
-from auth_routes import get_current_user
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException
+from auth_routes import oauth2_scheme
 from fastapi import Depends
-from models import User
+from fastapi import status
+from models import User, Order
+from schemas import OrderModel
+from jwt import decode
+from config import SECRET
+from database import Session, engine
+from fastapi.encoders import jsonable_encoder
+
 order_route = APIRouter(
     prefix="/orders",
     tags=["orders"]
 )
 
+session = Session(bind=engine)
 
-@order_route.get("/order")
-async def hell(data: User = Depends(get_current_user)):
+
+@order_route.get("/")
+async def hell(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        current_user = decode(token, key=SECRET, algorithms="HS256")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
     return {"message": "Hello Order"}
+
+
+@order_route.post("/order")
+async def place_an_order(order: OrderModel, token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        current_user = decode(token, key=SECRET, algorithms="HS256")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
+
+    user = session.query(User).filter(User.username == current_user["username"]).first()
+
+    new_order = Order(
+        pizza_sizes=order.pizza_sizes,
+        quantity=order.quantity,
+        user_id=current_user["sub"],
+    )
+    new_order.user = user
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "pizza_sizes": new_order.pizza_sizes,
+        "quantity": new_order.quantity,
+        "id": new_order.id,
+        "order_status": new_order.order_status
+    }
+
+    return jsonable_encoder(response)
